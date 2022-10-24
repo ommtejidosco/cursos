71b4c9a9501f6550001b8364c1e2caa2b3f73589
William Molina <wdmolina@bancolombia.com.co>
2022-10-24 18:28:08 -0500

    Improve service worker fetch in slow connection

--- sw.js
+++ sw.js
@@ -1,55 +1,71 @@
-self.addEventListener('install', function(event) {
-    self.skipWaiting();
-    event.waitUntil(
-      caches.open('main').then(function(cache) {
-        return cache.addAll(
-          [
-            '/',
-            '/manifest.json',
-            '/index.html',
-            '/icon-omm.webp'
-          ]
-        );
-      })
-    );
+self.addEventListener('install', function (event) {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open('main').then(function (cache) {
+      return cache.addAll(
+        [
+          '/',
+          '/manifest.json',
+          '/index.html',
+          '/icon-omm.webp'
+        ]
+      );
+    })
+  );
 });
 
+function networkOnly(event){
+  return fetch(event.request)
+}
+
 /* Network first */
-function networkFirst(event){
-    event.respondWith(
-        fetch(event.request).then(function(response){
-          return caches.open('main').then(function(cache){
-              cache.put(event.request, response.clone());
-              return response;
-          })
-        }).catch(function() {
-            return caches.match(event.request);
-        })
-    )
+function networkFirst(event) {
+  event.respondWith(
+    fetchWithTimeout(event.request).then(function (response) {
+      return caches.open('main').then(function (cache) {
+        cache.put(event.request, response.clone());
+        return response;
+      })
+    }).catch(function () {
+      return caches.match(event.request);
+    })
+  )
 }
 /* Cache first */
-function cacheFirst(event){
+function cacheFirst(event) {
   event.respondWith(
-    caches.open('main').then(function(cache) {
-      return cache.match(event.request).then(function(cacheResponse) {
-        if(cacheResponse)
+    caches.open('main').then(function (cache) {
+      return cache.match(event.request).then(function (cacheResponse) {
+        if (cacheResponse)
           return cacheResponse
         else
-          return fetch(event.request).then(function(networkResponse) {
+          return fetchWithTimeout(event.request).then(function (networkResponse) {
             cache.put(event.request, networkResponse.clone())
-              return networkResponse
+            return networkResponse
           })
       })
     })
   );
 }
 
+async function fetchWithTimeout(request, options = {}) {
+  const { timeout = 5000 } = options;
+  const controller = new AbortController();
+  const id = setTimeout(() => {console.log('Timeout'); controller.abort();}, timeout);
+  const response = await fetch(request, {
+    ...options,
+    signal: controller.signal
+  });
+  clearTimeout(id);
+  return response;
+}
+
 self.addEventListener('fetch', function (event) {
-    if(event.request.url.includes('resources') 
+  if (event.request.url.includes('resources')
     || event.request.url.includes('courses/espejo-renacer.webp'))
-      return cacheFirst(event);
-    else if(event.request.url.includes('.mp4')){
-      return fetch(event.request)
-    }else
-      return networkFirst(event);
+    return cacheFirst(event);
+  else if (event.request.url.includes('.mp4')) {
+    return networkOnly(event)
+  } else
+    return networkFirst(event);
 });
