53e4ab5d710a669d9cdb5e64da5e7fda44b36dad
William Molina <wdmolina@bancolombia.com.co>
2022-10-30 19:57:21 -0500

    Fix desktop contact link

--- contact/contact.js
+++ contact/contact.js
@@ -8,7 +8,7 @@ function loadComponentStyle(){
 function createComponent(){
     let isMobile = window.matchMedia("(any-pointer:coarse)").matches;
     let contactElement = document.createElement('a');
-    contactElement.href = (isMobile?'whatsapp://':'https://api.whatsapp.com') + 'send?phone=573176826533';
+    contactElement.href = (isMobile?'whatsapp://':'https://api.whatsapp.com/') + 'send?phone=573176826533';
     contactElement.innerHTML = `<div class='contact'></div>`;
     document.body.appendChild(contactElement);
 }
