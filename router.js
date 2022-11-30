d7b7e3fa46c0440122a2307df80423ff6115dd43
William Molina <wdmolina@bancolombia.com.co>
2022-11-30 16:36:25 -0500

    Enable video-able component

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const router = new Navigo("/");
 let component = {
     'coursesRoute': ['/components/courses/courses.js'],
-    'openCourse': ['/components/course/course.js?v=1.0.1']
+    'openCourse': ['/components/course/course.js?v=1.0.1','/components/video-able/video-able.js?v=0.0.2']
 }
 
 router.hooks({
